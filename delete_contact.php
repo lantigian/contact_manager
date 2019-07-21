372d91bcfce50d92bb140d436c0f53858e54cbb4
IanCarlos <iancarlos@IanCarloss-MacBook-Pro.local>
2019-07-20 20:30:52 -0500

    The funtionality of deleting a contact as well as redirecting to the show_contact.php page.

--- delete_contact.php
+++ delete_contact.php
@@ -4,27 +4,6 @@ $contact_id = $_GET['contact_id'];
 
 include('db.php');
 
-header ('Location:http://localhost/contact_manager/new_contact.php');
-
-
-$sql = "SELECT * FROM `contacts` WHERE `id`= '$contact_id' ORDER BY `id` DESC limit 1";
-
-$result = mysqli_query ($conn, $sql) or die ("Bad Query: $sql");
-while ($show = mysqli_fetch_assoc ($result)) 
-
-{
-
-   $contact_id = $show['id'];
-   $full_name = $show['full_name'];
-
-	$age = $show['age'];
-
-	$phone_number = $show['phone_number'];
-	
-	$email = $show['email'];
-}
-
-
 $sql = "DELETE FROM `contacts` WHERE `id`= '$contact_id' limit 1" ;
 
 echo $sql."\n";
@@ -37,28 +16,7 @@ else
   
 }
 
+header ("Location:http://localhost/contact_manager/new_contact.php?contact_id=".$contact_id );
 
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-
-<a href="http://localhost/contact_manager/show_contact.php?contact_id=<?php echo $conact_id; ?>" class="button"> Here is your newly created contact: </a>
-
-<br>
-			<h3> Here is that record you deleted </h3>
-		Full Name: <?php echo $full_name ?>
-	<br>
-		Age: <?php echo $age ?>
-	<br> 
-		email: <?php echo $email ?>
-	<br>
-		Phone Number: <?php echo $phone_number ?>
-	</br>
-
-</body>
-</html>

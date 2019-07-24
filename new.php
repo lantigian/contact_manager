c5616f8127315507ecd9da1b3e8ead8f38a86f0e
IanCarlos <iancarlos@IanCarloss-MacBook-Pro.local>
2019-07-23 19:47:43 -0500

    added css to the newly placed html label sections via the style section as well as changed the name of the file that utilizes the form since that file had undergone a name change prior not yet reflected.

--- new.php
+++ new.php
@@ -1,12 +1,18 @@
 <!DOCTYPE HTML>
 
-<html>
+<html lang="en">
 <body>
 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Basic Bootstrap Template</title>
+    <!-- Bootstrap CSS file -->
+    <link rel="stylesheet" href="js/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 <title> Assignment Zero </title>
-<h1> NEW CONTACT </h1>
-<form action= "create_contact.php" method = "POST">
-	full_name: <br>
+<h2> NEW CONTACT </h2>
+<form action= "create.php" method = "POST">
+	<label> full_name: <br>
 	<input type = 'text' name='full_name' placeholder="Mickey Rat"><br>
 	age: <br>
 	<input type = 'text' name='age' placeholder="18"><br>
@@ -14,10 +20,15 @@
 	<input type="text" name='phone_number' placeholder="xxx-xxx-xxxx"><br>
 	email: <br>
 	<input type="text" name="email" placeholder= 'neal4@gmail.com' ><br>
+	</label> 
 
 <input type = 'submit' value="'submit">
 </form>
+<style > 
 
+	form label {font-weight:bold}
+
+</style>
 
 
 </body>

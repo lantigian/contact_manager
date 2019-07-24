b7b8dc282a723824bc88b97c52d1488222394857
IanCarlos <iancarlos@IanCarloss-MacBook-Pro.local>
2019-07-24 16:28:21 -0500

    changed the echo message temporarily to test.

--- db.php
+++ db.php
@@ -10,6 +10,6 @@ $conn = new mysqli ($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "";
 
 ?>

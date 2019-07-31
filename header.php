921d54124f2e110a3782bd1c656fdb3ecdb839df
IanCarlos <iancarlos@IanCarloss-MBP.fios-router.home>
2019-07-30 20:04:10 -0400

    refactor bopotstrap locations of files

--- header.php
+++ header.php
@@ -2,13 +2,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>Bootstrap 4 Tutorial</title>                                       <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
+  <title> Contacts </title>                                       <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
 <link rel="stylesheet" href="stylesheets_bootstrap_4.3/custom.css">
+<link rel="stylesheet" href="stylesheets_bootstrap_4.3/bootstrap.css">
 <link rel="stylesheet" href="stylesheets_bootstrap_4.3/bootstrap-responsive.min.css">
 <link rel="stylesheet" href="stylesheets_bootstrap_4.3/bootstrap-responsive.css">
-<link rel="stylesheet" href="js/bootstrap.min.js">
-<link rel="stylesheet" href="js/bootstrap.js">
-<link rel="stylesheet" href="js/jquery.min.js">
-<link rel="stylesheet" href="js/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="javascript/bootstrap.min.js">
+<link rel="stylesheet" href="javascript/bootstrap.js">
+<link rel="stylesheet" href="javastript/jquery.min.js">
+<link rel="stylesheet" href="javascript/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="javastript/jquery.tabledit.js">
 </head>
  

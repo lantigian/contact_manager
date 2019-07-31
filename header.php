f929937d355743f33b10c48cf3ed952c1eb35f8a
IanCarlos <iancarlos@IanCarloss-MBP.fios-router.home>
2019-07-31 13:35:40 -0400

    added lines to refer to the bootstrap directory paths.

--- header.php
+++ header.php
@@ -3,14 +3,19 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <title> Contacts </title>                                       <link rel="stylesheet" href="stylesheets/bootstrap.min.css">
-<link rel="stylesheet" href="stylesheets_bootstrap_4.3/custom.css">
-<link rel="stylesheet" href="stylesheets_bootstrap_4.3/bootstrap.css">
-<link rel="stylesheet" href="stylesheets_bootstrap_4.3/bootstrap-responsive.min.css">
-<link rel="stylesheet" href="stylesheets_bootstrap_4.3/bootstrap-responsive.css">
-<link rel="stylesheet" href="javascript/bootstrap.min.js">
-<link rel="stylesheet" href="javascript/bootstrap.js">
-<link rel="stylesheet" href="javastript/jquery.min.js">
-<link rel="stylesheet" href="javascript/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<link rel="stylesheet" href="stylesheets/bootstrap_4.3.1/bootstrap.css">
+<link rel="stylesheet" href="stylesheets/bootstrap_4.3.1/bootstrap.min.css">
+<link rel="stylesheet" href="stylesheets/bootstrap_4.3.1/bootstrap-grid.css">
+<link rel="stylesheet" href="stylesheets/bootstrap_4.3.1/bootstrap-grid.min.css">
+<link rel="stylesheet" href="stylesheets/bootstrap_4.3.1/bootstrap-reboot.css">
+<link rel="javascript" href="javascript/bootstrap_4.3.1/bootstrap.min.js">
+<link rel="javascript" href="javascript/bootstrap_4.3.1/bootstrap.js">
+<link rel="javascript" href="javascript/bootstrap_4.3.1/bootstrap.bundle.js">
+<link rel="javascript" href="javascript/bootstrap_4.3.1/bootstrap.bundle.min.js">
+<link rel="javascript" href="javastript/bootstrap_4.3.1/jquery.min.js">
+<link rel="javascript" href="javastript/bootstrap_4.3.1/popper.min.js">
+<link rel="javascript" href="javascript/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="javastript/jquery.tabledit.js">
 </head>
  

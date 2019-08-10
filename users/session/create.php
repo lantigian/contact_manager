644b4597f75f83c56c9eaa0feb4a7476b4b01960
IanCarlos <iancarlos@IanCarloss-MBP.fios-router.home>
2019-08-10 15:33:45 -0400

    login with session was created and will include access to protected pages within the folloing commits. currently a user is directed to the index of contacts which will be only accesible to a user whom is registered once it becomes password protected. this is the start of a bigger scope.

--- users/session/create.php
+++ users/session/create.php
@@ -1,10 +1,22 @@
 <?php
-//check_pass and set_session page
+include ('/Applications/XAMPP/xamppfiles/htdocs/contact_manager/header.php'); ?>
+
+<?php
 session_start();
+if (! empty($_POST) ) {
+	if (isset($_POST['email']) && isset ($_POST['password'])) {
+		include ('/Applications/XAMPP/xamppfiles/htdocs/contact_manager/db.php');
+		$stmt = $conn->prepare("SELECT * FROM `users` WHERE `email` = ?");
+		$stmt ->bind_param('s', $_POST['email']);
+		$stmt-> execute();
+		$result = $stmt->get_result();
+		$user = $result->fetch_object();
 
-
-include ('/Applications/XAMPP/xamppfiles/htdocs/contact_manager/db.php');
-
-
+		if (password_verify($_POST['password'], $user->password)) {
+			$_SESSION['id'] = $user->ID;
+		}
+	}
+} 
+	header ("Location: http://localhost/contact_manager/contacts/index.php?sign_in=success"); 
 
 ?>
--- users/session/new.php
+++ users/session/new.php
@@ -1,3 +1,6 @@
+<?php 
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -12,24 +15,7 @@ include ('/Applications/XAMPP/xamppfiles/htdocs/contact_manager/header.php');
 <div id="frm">
 <h2> Signing in<h2>
 <form action= "create.php" method = "POST">
-	<p>
-	<div class="form-group">
-		<label> 
-
-			<b>first_name:</b>
-		</label>
-		<input type = 'text' name='full_name' placeholder="Ian">
-	</p>
-	</div>
-
-<p>
-		<div class="form-group">
-		<label> 
-			<b>last_name:<a>
-		</label>
-		<input type = 'text' name='last_name' placeholder="Lantigua">
-	</div>
-</p>
+	
 <p>	
 	<div class="form-group">
 		<label> 
